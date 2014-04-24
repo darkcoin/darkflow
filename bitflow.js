d152f3a164d0aa38da1270592d9a30f2624e95b1
Braydon Fuller <courier@braydon.com>
2014-04-23 17:38:51 -0700

    cleanup

--- bitflow.js
+++ bitflow.js
@@ -2,12 +2,7 @@ var bitflow = require('./bitflow-server');
 
 // start the application
 var bitflow = new bitflow.BitflowServer({
-    port : 1337, // port for the web server
+    port : 8080, // port for the web server
     debug : false, // true or false
     network : 'livenet', // livenet or testnet
 })
-
-var memory_interval = setInterval(function(){
-    console.log( process.memoryUsage() );
-}, 1000);
-
